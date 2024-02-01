a1bb7dba2cc5be1fcaaa592ca0ac4a81448733f7
Your Name <you@example.com>
2024-02-02 00:38:26 +0800

    R29kLW1vZGUK

---
+++ Writeups/js/arrowIcon.6ee2deee.js
@@ -0,0 +1,1 @@
+import{_ as o,o as e,c as t,a as n}from"./entry.37e139cb.js";const s={},_={xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 24 24"},c=n("path",{d:"M12 5V19M12 5L6 11M12 5L18 11","stroke-width":"2","stroke-linecap":"round","stroke-linejoin":"round"},null,-1),r=[c];function a(i,d){return e(),t("svg",_,r)}const p=o(s,[["render",a]]);export{p as _};
